c4d74f1352e221aec00ce39c35a34738a12d5589
Alejandro <Aleguemad94@yahoo.es>
2021-02-27 22:24:19 +0000

    FINAL

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,20 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 import PropType from "prop-types";
 import { Numeros } from "./numeros.js";
+import "./icons.js";
+
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
-const NUMERITO = () => {
-	setInterval(function() {
-		return <Numeros numero={"1"} />;
-	}, 3000);
-};
-
 //create your first component
+
 export function Home() {
-	return (
-		<div>
-			<Numeros />
-		</div>
-	);
+	return <div></div>;
 }
